3fef49df1cf12980b90234cd4b552a85fe3a593b
Jackson <jacksonboss88@gmail.com>
2024-06-10 14:45:46 -0500

    index.html

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
+const section = document.querySelector("section")
 const container = document.querySelector("#container");
 
+
 for (let i = 0; i < 256; i++) {
-    const div1 = document.createElement("div");
-    container.append(div1);
+    const divs = document.createElement("div");
+    divs.addEventListener('mouseover', () => {
+        divs.style.backgroundColor = 'orange';
+    });
+    container.append(divs);
 }
