3185b4466c98c98006a3beef84b968cd40bae4b9
Jackson <jacksonboss88@gmail.com>
2024-06-17 13:49:07 -0500

    index.html

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
-const section = document.querySelector("section")
+const section = document.querySelector("section");
 const container = document.querySelector("#container");
-
+const divContainer = document.querySelector('.divContainer');
+const btnClear = document.querySelector('button');
 
 for (let i = 0; i < 256; i++) {
-    const divs = document.createElement("div");
+    const divs = document.createElement('div');
     divs.addEventListener('mouseover', () => {
         divs.style.backgroundColor = 'orange';
     });
-    container.append(divs);
+    divContainer.append(divs);
 }
+
+function getResult() {
+    btnClear.addEventListener('click', () => {
+        const result = prompt('What grid size would you like?');
+        divContainer.remove('div');
+        const secondContainer = document.createElement('container');
+        const secondDivs = document.createElement('div');
+        secondContainer.classList.add('secondContainer');
+        container.append(secondContainer);
+        secondContainer.append(secondDivs);
+    });
+} 
+getResult();
