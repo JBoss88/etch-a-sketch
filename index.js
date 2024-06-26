a0c5faa5c60ef3aad67739f0bdba8ad5476d425e
Jackson <jacksonboss88@gmail.com>
2024-06-26 11:19:23 -0500

    js

--- index.js
+++ index.js
@@ -1,28 +1,45 @@
-const sizeOfGrid = 16;
 const container = document.querySelector('.container');
+const resetButton = document.querySelector('button');
 
 const createGrid = (amtOfGrids) => {
+    const wrapper = document.createElement('div');
+    wrapper.classList.add('wrapper');
+
     for (let i = 0; i < amtOfGrids; i++) {
         const row = document.createElement('div');
         row.classList.add('grid-row');
 
         for (let j = 0; j < amtOfGrids; j++) {
+            const widthAndHeight = 960 / amtOfGrids;
             const gridBox = document.createElement('div');
             gridBox.classList.add('grid-box');
-            row.appendChild(gridBox);
+            gridBox.style.width = `${widthAndHeight}px`;
+            gridBox.style.height = `${widthAndHeight}px`;
+
             gridBox.addEventListener('mouseover', () => {
-                gridBox.style.backgroundColor = 'orange';
+                gridBox.style.backgroundColor = 'orange'
             });
+            row.appendChild(gridBox);
         }
-        container.appendChild(row);
+
+        wrapper.appendChild(row);
     }
-}  
-createGrid(sizeOfGrid);
-
-
-
-
-function asksUser() {
-    const aNumber = Number(window.prompt('What grid size would you like?', ''));
-    console.log(aNumber)
+    container.appendChild(wrapper);
 }
+
+resetButton.addEventListener('click', () => {
+    let userSize = Number(prompt('What grid size would you like?'));
+
+    while (userSize > 100) {
+        userSize = Number(prompt("Pick a smaller number and make sure it's 100 or less."));
+    }
+
+    const wrapper = document.querySelector('.wrapper');
+
+    if (!wrapper) {
+        createGrid(userSize);
+    } else {
+        wrapper.remove();
+        createGrid(userSize);
+    }
+});
